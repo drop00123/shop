1e595d9914e392c5cc8d19997520e37757852e8a
Mike-Qian <1637010968@qq.com>
2021-05-01 20:24:45 +0800

    for循环

--- src/Demo.java
+++ src/Demo.java
@@ -2,7 +2,7 @@ public class Demo {
     public static void main(String[] args) {
         System.out.println("我的github设置");
         for (int i = 0; i < 10; i++) {
-            System.out.println(i);
+            System.out.println("第"+i);
         }
     }
 }
